9f5d412197e526bc95b53ed901e2be7b9993d647
Joseph cohen <jojocohen57@gmail.com>
2020-10-06 19:59:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const pets = ["Milo", "Otis", "Garfield"];
 
 
-pets.append('Odie')
-pets.prepend('Odie')
-pets.pop()
-pets.shift()
+append.append('Odie')
+prepend.prepend('Odie')
+removelast.pop()
+removeFirst.shift()
